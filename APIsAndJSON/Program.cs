3a83b8f61042c7b7d83e4eb7d7b6a740d0a4b98e
robertsteele7 <atpcycle7@gmail.com>
2022-11-13 15:13:22 -0600

    assignment

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace APIsAndJSON
-{
+﻿using APIsAndJSON;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+namespace KanyeAndRon 
+{ 
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+            var quote = new QuoteGenerator();
+
+
+            for (int i = 0; i < 7; i++)
+            {
+
+                Console.WriteLine($"Kanye: {quote.KanyeQuote()}");
+                
+                Console.WriteLine($"Ron Swanson:{quote.RonQuote()}");
+
+                Console.WriteLine($"-----------------------------------------------------------");
+            }
         }
     }
 }
---
+++ APIsAndJSON/QuoteGenerator.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIsAndJSON
+{
+    public class QuoteGenerator
+    {
+        public string KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+        public string RonQuote()
+        { 
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+        }
+            
+    }
+}
